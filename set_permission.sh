8ee503ac7f6576b2f5cf4058ca67417b05f66ea9
swt02026 <swt02026@yahoo.com.tw>
2016-03-17 14:04:35 +0800

    Create set_permission.sh

---
+++ set_permission.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+chmod -R 755 ./volumes/var/www/html/xoops
+chmod -R 755 ./volumes/var/xoops_data
+chmod -R 755 ./volumes/var/xoops_lib
+chmod 777 ./volumes/var/xoops_data/caches
+chmod 777 ./volumes/var/xoops_data/caches/smarty_cache
+chmod 777 ./volumes/var/xoops_data/caches/smarty_compile
+chmod 777 ./volumes/var/xoops_data/caches/xoops_cache
+chmod 777 ./volumes/var/xoops_data/configs
+chmod 777 ./volumes/var/xoops_data/data
