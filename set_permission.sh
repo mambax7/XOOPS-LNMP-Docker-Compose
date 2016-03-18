cb643d68d2e0e8f8792a8501fe9a311cd82961ea
swt02026 <swt02026@yahoo.com.tw>
2016-03-18 09:13:14 +0800

    Update set_permission.sh

--- set_permission.sh
+++ set_permission.sh
@@ -8,3 +8,7 @@ chmod 777 ./volumes/var/xoops_data/caches/smarty_compile
 chmod 777 ./volumes/var/xoops_data/caches/xoops_cache
 chmod 777 ./volumes/var/xoops_data/configs
 chmod 777 ./volumes/var/xoops_data/data
+chmod 777 ./volumes/var/www/html/xoops/mainfile.php
+chmod 777 ./volumes/var/www/html/xoops/templates_c
+chmod 777 ./volumes/var/www/html/xoops/cache
+chmod 777 ./volumes/var/www/html/xoops/uploads
